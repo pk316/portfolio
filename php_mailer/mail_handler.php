3ddf151400e14efc8bf7aab40e08aa166ec1398e
pk316 <pr.khiev@gmail.com>
2018-02-26 17:17:34 -0800

    updated php mailer

--- php_mailer/mail_handler.php
+++ php_mailer/mail_handler.php
@@ -1,49 +1,42 @@
 <?php
 require_once('email_config.php');
 require('phpmailer/PHPMailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+// $mail->SMTPDebug = 3;           // 3 is Enable verbose debug output. Change to 0 to disable debugging output.
 
-$message = [];
-$output = [
-    'success' => null,
-    'messages' => []
+$message = [
+    // 'email' => $_POST['email'],
+    // 'subject' => $_POST['subject'],
+    // 'body' => $_POST['message']
 ];
+
+$output =[
+    'success' => null,
+    'message' => []
+];
+
+//Sanitize name field
 $message['name'] = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
 if(empty($message['name'])){
     $output['success'] = false;
-    $output['message'][] = 'missing name key';
-};
-
+    $output['messages'][] = 'missing name key';
+}
+//Validate email field
 $message['email'] = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
 if(empty($message['email'])){
     $output['success'] = false;
-    $output['message'][] = 'invalid email key';
-};
-
-
-$message['message'] = filter_var($_POST['message'], FILTER_SANITIZE_STRING); 
+    $output['messages'][] = 'invalid email key';
+}
+//Sanitize message
+$message['message'] = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
 if(empty($message['message'])){
     $output['success'] = false;
-    $output['message'][] = 'missing name key';
-};
-
-if ($output['success'] !== null) {
-    http_response_code(400);
-    echo json_encode($output);
-    exit();
-};
-
-$message['message'] = nl2br($message['message']);
-
-$mail = new PHPMailer;
-// $mail->SMTPDebug = 3;   
-$_POST['name'];  
-$_POST['email'];
-$_POST['message'];     // Enable verbose debug output. Change to 0 to disable debugging output.
+    $output['messages'][] = 'missing message key';
+}
 
 $mail->isSMTP();                // Set mailer to use SMTP.
 $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers.
 $mail->SMTPAuth = true;         // Enable SMTP authentication
-
 
 $mail->Username = EMAIL_USER;   // SMTP username
 $mail->Password = EMAIL_PASS;   // SMTP password
@@ -56,12 +49,13 @@ $options = array(
         'allow_self_signed' => true
     )
 );
+
 $mail->smtpConnect($options);
 $mail->From = $message['email'];  // sender's email address (shows in "From" field)
-$mail->FromName = $message['name'];  // sender's name (shows in "From" field)
-$mail->addAddress(EMAIL_USER, 'First Recipient');  // Add a recipient
+$mail->FromName = $message['name'];   // sender's name (shows in "From" field)
+$mail->addAddress("pkhiev.dev@gmail.com");  // Add a recipient
 //$mail->addAddress('ellen@example.com');                        // Name is optional
-$mail->addReplyTo('example@gmail.com');                          // Add a reply-to address
+$mail->addReplyTo($message['email'], $message['name']);                          // Add a reply-to address
 //$mail->addCC('cc@example.com');
 //$mail->addBCC('bcc@example.com');
 
@@ -69,15 +63,26 @@ $mail->addReplyTo('example@gmail.com');                          // Add a reply-
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'Here is the subject';
-$mail->Body    = $message['message']; 
-$mail->AltBody = htmlentities($message['message']); 
+$mail->Subject = $_POST['subject'];
+$mail->Body    = $message['message'];
+$mail->AltBody = htmlentities($message['message']);
+
+if ($output['success'] !== null) {
+    http_response_code(400);
+    echo json_encode($output);
+    exit();
+}
+
+$message['message'] = nl2br($message['message']);
 
 if(!$mail->send()) {
-    $output ['success'] = false;
-    $output ['messages'][] = $mail -> ErrorInfo;
+    $output['success'] = false;
+    $output['message'][] = $mail->ErrorInfo;
+    // echo 'Message could not be sent.';
+    // echo 'Mailer Error: ' . $mail->ErrorInfo;
 } else {
-    $output ['success'] = true;
+    $output['success'] = true;
+    // echo 'Message has been sent';
 }
-echo json_encode($output);
+print(json_encode($output));
 ?>
