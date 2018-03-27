d7796ae8cbd592176e0a22e745a10c3842d3f626
pk316 <pr.khiev@gmail.com>
2018-03-27 14:41:32 -0700

    revert

--- js/main.js
+++ js/main.js
@@ -11,67 +11,35 @@ $(function () {
  *  =======================================*/
 
 
-$('.contact-name').on('input', function () {
-    var input = $(this);
-    var is_name = input.val();
-    if (is_name) { input.removeClass("invalid").addClass("valid"); }
-    else { input.removeClass("valid").addClass("invalid"); }
-});
-
-$('.contact-email').on('input', function () {
-    var input = $(this);
-    var re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    var is_email = re.test(input.val());
-    if (is_email) { input.removeClass("invalid").addClass("valid"); }
-    else { input.removeClass("valid").addClass("invalid"); }
-});
-
-$('.contact-message').keyup(function (event) {
-    var input = $(this);
-    var message = $(this).val();
-    console.log(message);
-    if (message) { input.removeClass("invalid").addClass("valid"); }
-    else { input.removeClass("valid").addClass("invalid"); }
-});
-
 function onContactSubmit() {
-    
     $('#contact-form').on('submit', function () {
-        // e.preventDefault();
+        event.preventDefault();
 
-        // $('#submit-button').find('.icon-spin').css('display', 'inline-block');
+    $('#submit-button').addClass('fas fa-spinner').text('Sending...');
 
-        var form_data = $("#contact-form").serializeArray();
-        var error_free = true;
-        for (var input in form_data) {
-            var element = $("#contact-" + form_data[input]['name']);
-            var valid = element.hasClass("valid");
-            var error_element = $("span", element.parent());
-            if (!valid) { error_element.removeClass("error").addClass("error-show"); error_free = false; }
-            else { error_element.removeClass("error-show").addClass("error"); }
-        }
-        if (!error_free) {
-            event.preventDefault();
-        }
-        else {
-            $('#submit-button').find('.icon-spin').css('display', 'inline-block');
-            $.ajax({
-                type: "POST",
-                url: '../php_mailer/mail_handler.php',
-                data: $("#contact-form").serialize(),
-                success: function (result) {
-                    setTimeout(function () {
-                        $('.msg-sent').show()
-                        $('#submit-button').find('.icon-spin').css('display', 'none');
-                        $(".form-control").val("")
-                    }, 1000)
-                }
-            });
+    $.ajax({
+        type : "POST",
+        url : '../php_mailer/mail_handler.php',
+        data : $("#contact-form").serialize(),
+        success : function (result) { 
+            setTimeout(function () {
+                $('.msg-sent').show()
+                $('#submit-button').find('.icon-spin').css('display', 'none');
+                $(".form-control").val("")
+            }, 1000)
         }
     });
+})}
+    
+
+function demo() {
+    $("#page").change(function () {
+        if ($(this).val() !== '') {
+            window.location.href = $(this).val();
+        }
+        return false;
+    });
 }
-
-
 
 /* =========================================
  *  lightbox
